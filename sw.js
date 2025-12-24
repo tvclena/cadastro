d9c55b9d2f7da7533475e720b492d562a6b5aa18
tvclena <tvclena@gmail.com>
2025-12-24 17:09:25 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "streaming-adm-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./logo.png"
+      ])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
